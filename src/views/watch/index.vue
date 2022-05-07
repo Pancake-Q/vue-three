795d07a74724c3819e7fcd35f5763046bc734a2e
zhangqi <zhangqi2@okay.cn>
2022-05-07 23:58:43 +0800

    refactor:directory init

---
+++ src/views/watch/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<div></div>
+</template>
+<script lang='ts'>
+import { defineComponent, onMounted } from 'vue';
+const module = defineComponent({
+	name: '',
+	components: {},
+	setup() {
+		onMounted(() => null);
+		return {}
+	}
+})
+export default module;
+</script>
+<style scoped lang='less'>
+</style>
