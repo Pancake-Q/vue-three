997a86e7b096bea86aa7e49aaa6fdbc8a25ca6c9
zhangqi <zhangqi2@okay.cn>
2022-05-08 23:22:41 +0800

    feat:watch

--- src/views/watch/index.vue
+++ src/views/watch/index.vue
@@ -1,14 +1,28 @@
 <template>
-	<div></div>
+	<div>
+		<div>{{ count }}</div>
+		<button @click="countHandler"></button>
+	</div>
 </template>
 <script lang='ts'>
-import { defineComponent, onMounted } from 'vue';
+import { defineComponent, watch, ref } from 'vue';
 const module = defineComponent({
-	name: '',
+	name: 'watch',
 	components: {},
 	setup() {
-		onMounted(() => null);
-		return {}
+		const count = ref(0);
+		const countHandler = () => {
+			count.value = count.value + 1;
+		}
+		watch(() => {
+			console.log(count);
+			return count
+		}, (value, prevalue) => {
+			console.log(value, prevalue);
+		}, {
+			immediate: true,
+		})
+		return { count, countHandler }
 	}
 })
 export default module;
