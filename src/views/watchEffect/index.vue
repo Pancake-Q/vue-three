3d18288327b3f0cce8bce02706a8d7970ad44b2b
zhangqi <zhangqi2@okay.cn>
2022-05-09 23:00:07 +0800

    fix:watch watchEffect

---
+++ src/views/watchEffect/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<div>{{ count }}</div>
+		<button @click="countHandler"></button>
+	</div>
+</template>
+<script lang='ts'>
+import { defineComponent, onMounted, watchEffect, ref } from 'vue';
+const module = defineComponent({
+	name: 'watchEffect',
+	components: {},
+	setup() {
+		const count = ref(0);
+		const countHandler = () => {
+			count.value = count.value + 1;
+		}
+		watchEffect(() => {
+			return count
+		})
+		return { count, countHandler }
+	}
+})
+export default module;
+</script>
+<style scoped lang='less'>
+</style>
