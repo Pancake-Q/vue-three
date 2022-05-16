4d575d661608945943215f8f4573d3cd86c2e15a
zhangqi <zhangqi2@okay.cn>
2022-05-16 16:37:56 +0800

    add alias

---
+++ src/components/HelloWorld/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <button @click="changeCount">
+    hahaahaha
+    count is: {{ store.count }}, count2 is : {{ store.count2 }}
+  </button>
+</template>
+
+<script setup lang="ts">
+import { useStore } from '@/store/store'
+
+const store = useStore()
+function changeCount() {
+  console.log(store)
+  store.count++
+}
+</script>
